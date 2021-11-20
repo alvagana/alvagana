9c7367f1d8de884c69f386f65930faed7698b5bb
Alvin Agana <alvagana@ucdavis.edu>
2021-11-19 17:21:53 -0800

    sliding animation

--- des157a/studio3/script.js
+++ des157a/studio3/script.js
@@ -14,26 +14,44 @@
     let actionText2 = document.getElementById('action-text-2');
     let rollText1 = document.getElementById('roll-1');
     let rollText2 = document.getElementById('roll-2');
+    let st = document.querySelector("#special-text");
+    let sw = document.querySelector("#sliding-window");
+    let stPlayer = document.querySelector("#sliding-window span:first-child");
 
     var gameData = {
         dice: ['./images/1die.jpg', './images/2die.jpg', './images/3die.jpg', './images/4die.jpg', './images/5die.jpg', './images/6die.jpg'],
         players: ['Player 1', 'Player 2'],
         score: [30, 30],
-        roll1: 0,
-        roll2: 0,
+        rollA1: 0,
+        rollA2: 0,
+        rollB1: 0,
+        rollB2: 0,
         rollSumA: 0,
         rollSumB: 0,
+        bonusRollA: 0,
+        bonusRollB: 0,
         actionA: "",
         actionB: "",
+        specialA: "",
+        specialB: "",
         currentTurn: 0,
         turnPhase: 0,
         turnCount: 1,
         gameEnd: 0
     }
 
+
+    // testing
+    /*
+    let testButton = document.getElementById("testing");
+    testButton.addEventListener("click", function() {
+
+    })
+    */
+
     startGame.addEventListener("click", function() {
         //gameData.index = Math.round(Math.random());
-        playAudio("alvinSinging");
+        //playAudio("alvinSinging");
 
         gameControl.innerHTML = '<h2>Meoooowwwww!!!!</h2>';
         gameControl.innerHTML += '<button id="quit">Quit?</button>';
@@ -42,7 +60,6 @@
             img.style.display = "block";
             let down = false;
             setInterval(function() {
-                console.log(img.getAttribute("transform"));
                 if (down) {
                     img.style.transform = "scaleY(1)";
                     down = !down;
@@ -111,25 +128,19 @@
     }
 
     function showRollText() {
-        rollText1.textContent = gameData.rollSumA;
-        rollText2.textContent = gameData.rollSumB;
+        gameData.bonusRollA == 0 ? rollText1.textContent = `${gameData.rollA1} + ${gameData.rollA2}` : rollText1.textContent = `${gameData.rollA1} + ${gameData.rollA2} + ${gameData.bonusRollA}`;
+        gameData.bonusRollB == 0 ? rollText2.textContent = `${gameData.rollB1} + ${gameData.rollB2}` : rollText2.textContent = `${gameData.rollB1} + ${gameData.rollB2} + ${gameData.bonusRollB}`;
         rollText1.style.transform = "scaleY(100%) rotate(-5deg)";
         rollText1.style.transition = "transform 0.5s";
-        rollText2.style.transform = "scaleY(100%) rotate(-5deg)";
-        rollText2.style.transition = "transform 0.5s";
+        setTimeout(function() {
+            rollText2.style.transform = "scaleY(100%) rotate(-5deg)";
+            rollText2.style.transition = "transform 0.5s";
+        }, 1000);
     }
 
     function executeTurn() {
-        // Rolling
-        //setTimeout(function() {
-            gameData.roll1 = Math.floor(Math.random() * 6) + 1;
-            gameData.rollSumA = gameData.roll1 + gameData.roll2;
-        //}, 500);
-        //setTimeout(function() {
-            gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-            gameData.rollSumB = gameData.roll1 + gameData.roll2;
-            showRollText();
-        //}, 500);
+        rollDice();
+        showRollText();
 
         let scoreA = gameData.rollSumA;
         let scoreB = gameData.rollSumB;
@@ -140,7 +151,9 @@
             //game.textContent = `Turn ${gameData.turnCount}: Both defended! Nothing happened!`;
         } else {
             if (winner != 'TIE') {
-                performAction(winner);
+                setTimeout(function() {
+                    performAction(winner)
+                }, 1750);
                 //game.textContent = `Turn ${gameData.turnCount}: Player 1 rolled a ${gameData.rollSumA} and Player 2 rolled a ${gameData.rollSumB}. Player ${winner} wins the turn.`;
             } else {
                 //game.textContent = `Turn ${gameData.turnCount}: It was a TIE!`;
@@ -151,14 +164,63 @@
         gameData.turnCount += 1;
 
         // Check winning condition
-        checkWinningCondition();
         setTimeout(function() {
             rollText1.style.transform = "scale(0)";
             rollText2.style.transform = "scale(0)";
             actionText1.style.transform = "scale(0)";
             actionText2.style.transform = "scale(0)";
+            sw.style.animation = "none";
+            gameData.bonusRollA = 0;
+            gameData.bonusRollB = 0;
             game.textContent = `${gameData.players[gameData.currentTurn]}, what is your move?`;
+            checkWinningCondition();
         }, 2000);
+    }
+
+    function rollDice() {
+        gameData.rollA1 = Math.floor(Math.random() * 6) + 1;
+        gameData.rollA2 = Math.floor(Math.random() * 6) + 1;
+        gameData.rollSumA = gameData.rollA1 + gameData.rollA2;
+        checkSpecial(1);
+
+        gameData.rollB1 = Math.floor(Math.random() * 6) + 1;
+        gameData.rollB2 = Math.floor(Math.random() * 6) + 1;
+        gameData.rollSumB = gameData.rollB1 + gameData.rollB2;
+        checkSpecial(2);
+    }
+
+    function checkSpecial(player) {
+        let roll1;
+        let roll2;
+        
+        if (player == 1) {
+            roll1 = gameData.rollA1;
+            roll2 = gameData.rollA2;
+        } else {
+            roll1 = gameData.rollB1;
+            roll2 = gameData.rollB2;
+        }
+
+        player == 1 ? stPlayer.textContent = `Player 1:` : stPlayer.textContent = `Player 2:`;
+       
+        if (roll1 == roll2 && roll1 != 1) {
+            st.textContent = "DOUBLES";
+            sw.style.animation = "slide 2s";
+            let rand = Math.floor(Math.random() * 6) + 1;
+            player == 1 ? gameData.bonusRollA = rand : gameData.bonusRollB = rand;
+            player == 1 ? gameData.rollSumA += gameData.bonusRollA : gameData.rollSumB += gameData.bonusRollB;
+        } else if (roll1 == 1 && roll2 == 1) {
+            st.textContent = "MOUSE!!";
+            sw.style.animation = "slide 2s";
+            player == 1 ? gameData.rollA1 = 0 : gameData.rollB1 = 0;
+            player == 1 ? gameData.rollA2 = 0 : gameData.rollB2 = 0;
+            player == 1 ? gameData.rollSumA = 0 : gameData.rollSumB = 0;
+        } else if (roll1 + roll2 == 9) {
+            st.textContent = "9 LIVES";
+            sw.style.animation = "slide 2s";
+            player == 1 ? gameData.bonusRollA = 1 : gameData.bonusRollB = 1;
+            player == 1 ? gameData.rollSumA += gameData.bonusRollA : gameData.rollSumB += gameData.bonusRollB;
+        }
     }
 
     function updateHealthBar(player) {
@@ -197,7 +259,7 @@
         if (winner == 'A') {
             gameData.actionA == "scratch" ? gameData.score[1] -= gameData.rollSumA : gameData.score[0] = Math.min(gameData.rollSumA + gameData.score[0], 30);
         } else if (winner == 'B') {
-            gameData.actionB == "scratch" ? gameData.score[0] -= gameData.rollSumB : gameData.score[1] = Math.min(gameData.rollSumB + gameData.score[0], 30);
+            gameData.actionB == "scratch" ? gameData.score[0] -= gameData.rollSumB : gameData.score[1] = Math.min(gameData.rollSumB + gameData.score[1], 30);
         }
 
         updateHealthBar('A');
@@ -214,13 +276,13 @@
     */
 
     function checkWinningCondition() {
-        if (gameData.score[0] <= gameData.gameEnd || gameData.score[1] <= gameData.gameEnd) {
+        if (gameData.score[0] <= 0 || gameData.score[1] <= 0) {
             if (gameData.score[1] <= gameData.score[0]) {
                 game.textContent = `Player 1 wins!`;
             } else {
                 game.textContent = `Player 2 wins!`;
             }
-            game.textContent = `Game over!`;
+            //game.textContent = `Game over!`;
             actionArea.innerHTML = '';
             document.getElementById('quit').innerHTML = "Start a New Game?";
         }
