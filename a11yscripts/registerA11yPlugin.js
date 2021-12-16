7bad22fbc8201670b58f12b68179aeb9c1269de9
Alvin Agana <alvagana@ucdavis.edu>
2021-12-16 12:33:03 -0800

    edited typo

--- a11yscripts/registerA11yPlugin.js
+++ a11yscripts/registerA11yPlugin.js
@@ -4,6 +4,7 @@ import registerPlugin from './plugin';
 // Adds this plugin to the LibreEditor for later activation
 // this will ensure that `registerPlugin()` be called after
 // CKeditor is initialized on Libretexts page
-LibreEditor.queryPlugin = (config) => {
+LibreEditor.a11yplugin = (config) => {
   registerPlugin();
+  config.toolbar[12].push('a11ychecker');
 };
