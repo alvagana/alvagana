9f21c3edafc6cdead96c29f784427561e879471b
Alvin Agana <alvagana@ucdavis.edu>
2022-02-03 10:28:33 -0800

    finished studio3

--- des157b/studio3/script.js
+++ des157b/studio3/script.js
@@ -1,5 +1,6 @@
 
 const body = document.querySelector("body");
+const footer = document.querySelector("footer");
 var slideCount = 0;
 
 document.addEventListener('keydown', handleKey);
@@ -10,6 +11,14 @@ let secOneTextBaffle = baffle('#sec-one-text', {
 
 let noReveal = baffle('.text-no-reveal', {
     characters: '</>░▐░	█'
+})
+
+let buttonText = baffle('.button-no-reveal', {
+    characters: '░▐░█'
+})
+
+let faceCover = baffle('.polaroid.p-three span', {
+    characters: '▐█▙▜▟▓'
 })
 
 secOneTextBaffle.start();
@@ -43,8 +52,21 @@ function checkBaffle() {
         secOneTextBaffle.stop();
     }
 
+    if (slideCount == 2) {
+        buttonText.start();
+    }
+
+    if (slideCount == 3) {
+        buttonText.stop();
+        faceCover.start();
+    }
+
     if (slideCount == 6) {
         noReveal.stop();
+    }
+
+    if (slideCount == 8) {
+        footer.textContent = "thank you for reading :)"
     }
 }
 
@@ -55,4 +77,28 @@ function checkBackground() {
         body.style.transition = "0.5s right, 4s background-color"
     }
 }
+
+
+
+// text animation
+var textWrapper = document.querySelector('.ml13');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+    anime.timeline({loop: true})
+      .add({
+        targets: '.ml13 .letter',
+        translateY: [-100,0],
+        translateZ: 0,
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 1400,
+        delay: (el, i) => 300 + 30 * i
+      }).add({
+        targets: '.ml13 .letter',
+        translateY: [0,300],
+        opacity: [1,0],
+        easing: "easeInExpo",
+        duration: 2200,
+        delay: (el, i) => 100 + 30 * i
+      });
 
