bf9577ed06d58e7250b271941ea8e5bb3c274783
Alvin Agana <alvagana@ucdavis.edu>
2022-02-28 20:39:41 -0800

    v2 progress: grid

--- des157b/final/development/version2/questionnaire.js
+++ des157b/final/development/version2/questionnaire.js
@@ -5,11 +5,15 @@ let nameInput = document.querySelector("#name-input")
 let noteOptions = document.querySelectorAll(".note-option")
 let tapeOptions = document.querySelectorAll(".tape-option")
 let note = document.querySelector("#note");
+let tape = document.querySelector("#tape")
+let charCount = document.querySelector("#char-count")
 let noteColors = ["#E8DFF5", "#FCE1E4", "#FCF4DD", "#DDEDEA", "#DAEAF6"];
 let tapeColors = ["#00A5E3", "#8DD7BF", "#FF96C5", "#FF5768", "#FFBF65"];
 
 textArea.addEventListener("input", function() {
     noteContent.textContent = textArea.value;
+    charCount.textContent = textArea.value.length;
+
 })
 
 nameInput.addEventListener("input", function() {
@@ -33,5 +37,16 @@ noteOptions.forEach((noteOption, index) => {
 
 //TODO
 tapeOptions.forEach((tapeOption, index) => {
+    tapeOption.addEventListener("click", function() {
+        tape.removeAttribute("class");
+        tape.classList.add(`tape-color-${index+1}`)
+    })
 
+    tapeOption.addEventListener("mouseover", function () {
+        tapeOption.textContent = tapeColors[index]
+    })
+
+   tapeOption.addEventListener("mouseout", function() {
+        tapeOption.textContent = "";
+    })
 })
